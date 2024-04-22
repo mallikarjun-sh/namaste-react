1e25eede91044b14e53373538de87bf556cf8aa4
mallikarjun-sh <mallikarjun.sh@gmail.com>
2024-04-22 13:48:01 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,18 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//const heading = React.createElement("h1", {id:"heading"}, "Hello React!");
+const parent = React.createElement("div", { id: "parent" },
+    [
+        React.createElement("div", { id: "child1" },
+        [
+         React.createElement("h1", { id: "heading" }, "I am an H1 tag"), 
+         React.createElement("h2", {}, "I am an H2 Tag")
+        ]),
+        React.createElement("div", { id: "child2" },
+        [
+         React.createElement("h1", { id: "heading" }, "I am an H1 tag"), 
+         React.createElement("h2", {}, "I am an H2 Tag")
+        ])
+    ]
+)
+console.log(parent);
+root.render(parent);
